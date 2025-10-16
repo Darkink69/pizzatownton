3d008674d8cebb58d0ccfb8e7ca7b04b4e1a8b23
Vladimir Arkatov <chch69_for_trash@mail.ru>
2025-10-16 10:36:50 +0700

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl">pizzatownton</div>
+      <img
+        src="https://s3.twcstorage.ru/c6bae09a-a5938890-9b68-453c-9c54-76c439a70d3e/Pizzatownton/pizza_big_logo.png"
+        alt=""
+      />
+    </>
+  );
+}
+
+export default App;
