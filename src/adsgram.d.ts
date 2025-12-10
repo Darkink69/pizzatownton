20d01bce5d5b5aa4d4cde203b949cfa044ba286f
Darkink69 <chch69_for_trash@mail.ru>
2025-12-10 22:20:19 +0700

    tasks_0.71v

--- src/adsgram.d.ts
+++ src/adsgram.d.ts
@@ -10,50 +10,62 @@ declare module "react/jsx-runtime" {
     }
   }
 }
+// import { DOMAttributes } from "react";
 
-export interface ShowPromiseResult {
-  done: boolean; // true, если досмотрено до конца (rewarded) или закрыто (interstitial)
-  description: string;
-  state: "load" | "render" | "playing" | "destroy";
-  error: boolean; // true при событии ошибки
-}
+// type CustomElement<T> = Partial<T & DOMAttributes<T> & { children: any }> &
+//   LibraryManagedAttributes;
 
-interface AdsgramInitParams {
-  blockId: string; // уникальный идентификатор рекламного блока
-  debug?: boolean; // debug режим (опционально)
-  debugBannerType?: BannerType; // тип тестового баннера (опционально)
-}
+// declare module "react/jsx-runtime" {
+//   namespace JSX {
+//     interface IntrinsicElements extends JSXInternal {
+//       "adsgram-task": CustomElement<HTMLDivElement>;
+//     }
+//   }
+// }
 
-type EventType =
-  | "onReward" // пользователь получил награду за просмотр рекламы
-  | "onComplete" // пользователь досмотрел Interstitial баннер или закрыл его
-  | "onStart" // первый кадр баннера отображён
-  | "onSkip" // пользователь пропустил рекламу
-  | "onBannerNotFound" // баннер для показа отсутствует
-  | "onNonStopShow" // пользователь пытается посмотреть несколько реклам подряд
-  | "onTooLongSession" // слишком долгая сессия, необходимо перезапустить приложение чтобы получать рекламу
-  | "onError"; // ошибка при рендере или воспроизведении рекламы
-type HandlerType = () => void; // функция обратного вызова для событий
+// export interface ShowPromiseResult {
+//   done: boolean; // true, если досмотрено до конца (rewarded) или закрыто (interstitial)
+//   description: string;
+//   state: "load" | "render" | "playing" | "destroy";
+//   error: boolean; // true при событии ошибки
+// }
 
-export interface AdController {
-  show(): Promise<ShowPromiseResult>;
-  addEventListener(event: EventType, handler: HandlerType): void; // подписывает на событие
-  removeEventListener(event: EventType, handler: HandlerType): void; // отписывает от события
-  destroy(): void; // прекращает показ рекламы и очищает ресурсы
-}
+// interface AdsgramInitParams {
+//   blockId: string; // уникальный идентификатор рекламного блока
+//   debug?: boolean; // debug режим (опционально)
+//   debugBannerType?: BannerType; // тип тестового баннера (опционально)
+// }
 
-declare global {
-  interface Window {
-    Adsgram?: {
-      init(params: { blockId: string }): AdController;
-    };
-    Telegram?: {
-      WebApp?: {
-        initData?: string;
-        platform?: string;
-      };
-    };
-  }
-}
+// type EventType =
+//   | "onReward" // пользователь получил награду за просмотр рекламы
+//   | "onComplete" // пользователь досмотрел Interstitial баннер или закрыл его
+//   | "onStart" // первый кадр баннера отображён
+//   | "onSkip" // пользователь пропустил рекламу
+//   | "onBannerNotFound" // баннер для показа отсутствует
+//   | "onNonStopShow" // пользователь пытается посмотреть несколько реклам подряд
+//   | "onTooLongSession" // слишком долгая сессия, необходимо перезапустить приложение чтобы получать рекламу
+//   | "onError"; // ошибка при рендере или воспроизведении рекламы
+// type HandlerType = () => void; // функция обратного вызова для событий
 
-export {};
+// export interface AdController {
+//   show(): Promise<ShowPromiseResult>;
+//   addEventListener(event: EventType, handler: HandlerType): void; // подписывает на событие
+//   removeEventListener(event: EventType, handler: HandlerType): void; // отписывает от события
+//   destroy(): void; // прекращает показ рекламы и очищает ресурсы
+// }
+
+// declare global {
+//   interface Window {
+//     Adsgram?: {
+//       init(params: { blockId: string }): AdController;
+//     };
+//     Telegram?: {
+//       WebApp?: {
+//         initData?: string;
+//         platform?: string;
+//       };
+//     };
+//   }
+// }
+
+// export {};
--- src/components/showAdButton.tsx
+++ src/components/showAdButton.tsx
@@ -1,24 +1,24 @@
-import { useCallback } from "react";
-import { useAdsgram } from "./useAdsgram";
+// import { useCallback } from "react";
+// import { useAdsgram } from "./useAdsgram";
 
-export function ShowAdButton() {
-  const onReward = useCallback(() => {
-    alert("Reward");
-  }, []);
-  const onError = useCallback((result: any) => {
-    alert(JSON.stringify(result, null, 4));
-  }, []);
+// export function ShowAdButton() {
+//   const onReward = useCallback(() => {
+//     alert("Reward");
+//   }, []);
+//   const onError = useCallback((result: any) => {
+//     alert(JSON.stringify(result, null, 4));
+//   }, []);
 
-  /**
-   * Вставьте ваш blockId
-   */
-  const showAd = useAdsgram({ blockId: "task-18892", onReward, onError });
+//   /**
+//    * Вставьте ваш blockId
+//    */
+//   const showAd = useAdsgram({ blockId: "task-18892", onReward, onError });
 
-  return (
-    <button className="opacity-50" onClick={showAd}>
-      Show Ad
-    </button>
-  );
-}
+//   return (
+//     <button className="opacity-50" onClick={showAd}>
+//       Show Ad
+//     </button>
+//   );
+// }
 
-export default ShowAdButton;
+// export default ShowAdButton;
